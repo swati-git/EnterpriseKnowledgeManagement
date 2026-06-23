cfeca8426f69d1fb0963c338ef5479f4e2332a2a
space <swati.phartiyal@gmail.com>
2026-06-23 19:00:10 +0530

    Format the chain

--- src/graph_nodes/generate_llm_answer.py
+++ src/graph_nodes/generate_llm_answer.py
@@ -1,12 +1,18 @@
 from typing import Any, Dict
-from src.chains.llm_repsonse_generation_chain import llm_answer
+from src.chains.llm_repsonse_generation_chain import response_formatter
 from src.graph_state.state import GraphState
 
 
 def generate(state: GraphState) -> Dict[str, Any]:
     """Generate answer using documents and question."""
+    
     print("---GENERATE---")
+    
     question = state["query"]
     documents = state["documents"]
-    llm_generated_response = llm_answer.invoke({"documents": documents, "query": question})
-    return {"documents": documents, "query": question, "llm_generated_response": llm_generated_response}
+    llm_generated_response = response_formatter.invoke({"documents": documents, "query": question})
+    print(f"LLM Generated Response: {llm_generated_response}")
+
+    return {
+        "llm_generated_response": llm_generated_response
+        }
