f2d07d9dbc466480388600be4f068ca2f3dc98f6
space <swati.phartiyal@gmail.com>
2026-06-23 18:27:41 +0530

    Rename the nodes to sync with the node functionality

--- src/workflow/graph.py
+++ src/workflow/graph.py
@@ -3,15 +3,15 @@ from langgraph.constants import  END
 from src.graph_state.state import GraphState
 from src.chains.query_router import query_router, QueryRouter
 from src.graph_nodes.web_search import web_search
-from src.graph_nodes.retrieve import retrieve
+from src.graph_nodes.vectorstore import vectorstore
 from src.graph_nodes.grader import grader
 from src.graph_nodes.generate_llm_answer import generate
 
 
 workflow = StateGraph(GraphState)
 
-workflow.add_node("WEBSEARCH", web_search)
-workflow.add_node("RETRIEVE", retrieve)
+workflow.add_node("WEBSEARCH", web_search) # type: ignore
+workflow.add_node("VECTORSTORE", vectorstore)
 workflow.add_node("GRADER", grader)
 workflow.add_node("GENERATE_ANSWER", generate)
 
@@ -21,18 +21,20 @@ def route_query(state: GraphState) -> str:
     source: QueryRouter = query_router.invoke({"query": state["query"]})
     print(f"Routed to source: {source.source}")
     
-    return "WEBSEARCH" if source.source == "WEBSEARCH" else "RETRIEVE"
+    return "WEBSEARCH" if source.source == "WEBSEARCH" else "VECTORSTORE"
 
 workflow.set_conditional_entry_point(
     route_query,
     {
         "WEBSEARCH": "WEBSEARCH",
-        "RETRIEVE": "RETRIEVE",
+        "VECTORSTORE": "VECTORSTORE",
     },
 )
 workflow.add_edge("WEBSEARCH", "GRADER")
-workflow.add_edge("RETRIEVE", "GRADER")
+workflow.add_edge("VECTORSTORE", "GRADER")
 workflow.add_edge("GRADER", "GENERATE_ANSWER")
+workflow.add_edge("GENERATE_ANSWER", END)
+
 
 application = workflow.compile()
 
